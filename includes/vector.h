56050924058749c708e9eae4cdbf4664bbf50781
Hbprado <hbp@cin.ufpe.br>
2024-05-08 11:05:27 -0300

    feat: add operações básicas de vetor

---
+++ includes/vector.h
@@ -0,0 +1,24 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+class Point;
+class Vector
+{
+public:
+    double x, y, z;
+
+    Vector();
+    Vector(double _x, double _y, double _z);
+
+    // Operações básicas através da sobrecarga dos operadores
+    Vector operator+(const Vector &other) const;
+    Vector operator-(const Vector &other) const;
+    Vector operator*(double scalar) const;
+    // Demais operações entre vetores
+    double dot(const Vector &other) const;
+    Vector cross(const Vector &other) const;
+    double length() const;
+    Vector normalize() const;
+};
+
+#endif
---
+++ src/vector.cpp
@@ -0,0 +1,50 @@
+#include "../includes/vector.h"
+#include <cmath>
+#include <iostream>
+
+Vector::Vector() : x(0.0), y(0.0), z(0.0) {}
+
+Vector::Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {}
+
+Vector Vector::operator+(const Vector &other) const
+{
+    return Vector(x + other.x, y + other.y, z + other.z);
+}
+
+Vector Vector::operator-(const Vector &other) const
+{
+    return Vector(x - other.x, y - other.y, z - other.z);
+}
+
+Vector Vector::operator*(double scalar) const
+{
+    return Vector(x * scalar, y * scalar, z * scalar);
+}
+
+double Vector::dot(const Vector &other) const
+{
+    return x * other.x + y * other.y + z * other.z;
+}
+
+Vector Vector::cross(const Vector &other) const
+{
+    return Vector(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);
+}
+
+double Vector::length() const
+{
+    return std::sqrt(x * x + y * y + z * z);
+}
+
+Vector Vector::normalize() const
+{
+    double len = length();
+    if (len != 0.0)
+    {
+        return *this * (1.0 / len);
+    }
+    else
+    {
+        return Vector();
+    }
+}
